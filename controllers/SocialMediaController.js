e3fa5d49e7d58302cdee4f6b495ba82c18ab324d
IKLANLO <ilandaberea@yahoo.es>
2024-01-16 19:36:46 +0100

    añadido bcryptjs a createUser, y validación de usuario existente

--- controllers/SocialMediaController.js
+++ controllers/SocialMediaController.js
@@ -3,11 +3,19 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { jwt_secret } = require('../config/keys')
 
+async function checkUser(req){
+  return await SocialMedia.findOne({email: req})
+}
+
 const SocialMediaController = {
   async createUser(req, res){
     try {
-      await SocialMedia.create(req.body)
-      res.status(201).send({message: 'new user created successfully!!!'})
+      const check = await checkUser(req.body.email)
+      if(check) return res.status(500).send({message: 'User already exists, choose another email'})
+      if(!req.body.password) res.status(400).send({message: 'Password required'})
+      const password = bcrypt.hashSync(req.body.password, 10)
+      const user = await SocialMedia.create({...req.body, password})
+      res.status(201).send({message: 'new user created successfully', user})
     } catch (error) {
       console.error(error)
       res.status(500).send({message: 'error creating the new user'})
@@ -27,10 +35,8 @@ const SocialMediaController = {
 
   async login(req, res){
     try {
-      const user = await SocialMedia.findOne({
-        email: req.body.email
-      })
-      
+      const user = await checkUser(req.body.email)
+      if (!user) return res.status(500).send({message: `User doesn't exists, choose another email`})
       const token = jwt.sign({_id: user._id}, jwt_secret)
       if(user.tokens.length > 4) user.tokens.shift()
       user.tokens.push(token)
