dcc64e60b8843c0850227964fed51fb8e6cd03e9
IKLANLO <ilandaberea@yahoo.es>
2024-02-29 23:01:17 +0100

    añadido cors

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8081
 const { dbConnection } = require('./config/config')
+const cors = require('cors')
 
 app.use(express.json())
+
+app.use(cors())
 
 dbConnection()
 
